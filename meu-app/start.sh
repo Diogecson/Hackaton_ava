ba6d86ba7cae1643a229ce7afb66ef862fd0a16e
Diogecson <diogecson@gmail.com>
2025-06-30 17:49:15 -0300

    atualização

--- meu-app/start.sh
+++ meu-app/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=true
 
-# Instalar o Chrome headless
-apt-get update && apt-get install -y wget unzip curl
+# Instalar Chrome headless
+apt-get update && apt-get install -y wget unzip curl gnupg
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb || true
 
